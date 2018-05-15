b19438a3d59f42378222d5c5a6ef974ed426e847
Alex Leontiev <inp9822058@alumni.cs.nctu.edu.tw>
2018-05-15 22:50:07 +0800

    commit

--- habits.scm
+++ habits.scm
@@ -12,15 +12,17 @@
 (makehabits "10:00" "13:00" 1 "einsoch6 makedocs" 1080 '(category "german") '(info "http://www.dw.com/de/deutsch-lernen/das-bandtagebuch-1-lass-uns-reden/s-32658"))
 (makehabits "10:00" "13:00" 2 "einsoch6 translate" 1080 '(category "german") '(info "http://www.dw.com/de/deutsch-lernen/das-bandtagebuch-1-lass-uns-reden/s-32658"))
 (makehabits "10:00" "13:00" 6 "einsoch6 ubung" 1080 '(category "german") '(info "http://www.dw.com/de/deutsch-lernen/das-bandtagebuch-1-lass-uns-reden/s-32658"))
-(makehabits "13:00" "16:00" 6 "pluralization flashcards" 1080 '(info "https://tiny.cards/decks/ed6eddd0-274f-4ac4-a96a-70f3f0e08726")'(category "german"))
-(makehabits "13:00" "16:00" 4 "germ sentences" 1080 '(info "https://tiny.cards/decks/5oPZSSoF/german-sentences")'(category "german"))
-(makehabits "16:00" "19:00" 4 "5 flashcards make" 1080  
-	    (list 'info wordcardurl)
-	    '(category "german"))
-(makehabits "16:00" "19:00" 4 "5 flashcards prac" 1080  
-	    (list 'info wordcardurl)
-	    '(category "german"))
-(makehabits "19:00" "22:00" 1 "german workbook 1 unit and FCsentences" 1080 '(category "german"))
+
+;(makehabits "13:00" "16:00" 6 "pluralization flashcards" 1080 '(info "https://tiny.cards/decks/ed6eddd0-274f-4ac4-a96a-70f3f0e08726")'(category "german"))
+;(makehabits "13:00" "16:00" 4 "germ sentences" 1080 '(info "https://tiny.cards/decks/5oPZSSoF/german-sentences")'(category "german"))
+
+;(makehabits "16:00" "19:00" 4 "5 flashcards make" 1080  
+;	    (list 'info wordcardurl)
+;	    '(category "german"))
+;(makehabits "16:00" "19:00" 4 "5 flashcards prac" 1080  
+;	    (list 'info wordcardurl)
+;	    '(category "german"))
+;(makehabits "19:00" "22:00" 1 "german workbook 1 unit and FCsentences" 1080 '(category "german"))
 (flushhabits)
 
 (makehabit "0 7 * * *" "pullups 10:1" 1080 '(category "sport"))
